7c647be8c32b2a2b6162de4410a8413e6a9a38af
I066841 <a.kulshreshtha@sap.com>
2019-08-17 01:09:30 +0530

    Ternary Operators

--- src/com/learn/java8/main/myComparator.java
+++ src/com/learn/java8/main/myComparator.java
@@ -6,13 +6,15 @@ public class myComparator implements Comparator<Integer> {
 
 	public int compare(Integer a, Integer b) {
 
-		if (a > b) {
-			return -1;
-		} else if (a < b) {
-			return 1;
-		} else {
-			return 0;
-		}
+//		if (a > b) {
+//			return -1;
+//		} else if (a < b) {
+//			return 1;
+//		} else {
+//			return 0;
+//		}
+		
+		return (a > b)?-1: (a < b)?1: 0;
 
 	}
 

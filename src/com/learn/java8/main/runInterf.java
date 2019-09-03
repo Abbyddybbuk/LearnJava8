ced78c2a07f60b4e709cfef2220159928004bb3b
I066841 <a.kulshreshtha@sap.com>
2019-09-04 00:51:13 +0530

    Execute Interface directly

---
+++ src/com/learn/java8/main/runInterf.java
@@ -0,0 +1,9 @@
+package com.learn.java8.main;
+
+public interface runInterf {
+	public static void main(String[] args) {
+//		With Java 1.8 version, interface can be executed directly as they can contain static methods which include main() method as well
+		System.out.println("Executing the interface directly");
+
+	}  
+}

b0673c32bda5464b949539232ac63c3879a8297f
I066841 <a.kulshreshtha@sap.com>
2019-09-05 10:23:01 +0530

    Predicate Definition

---
+++ src/com/learn/java8/main/PredicateEg1.java
@@ -0,0 +1,19 @@
+package com.learn.java8.main;
+
+// Predicate 
+// 1. Introduced in 1.8 v of java
+// 2. present in java.util.function
+// 3. contains only test() method
+// 4. interface Predicate<T> {
+//        boolean test(T t) {
+//      }
+//    }
+
+public class PredicateEg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

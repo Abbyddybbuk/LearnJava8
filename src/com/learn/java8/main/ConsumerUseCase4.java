b619af8e5036fdebbd051bb6a2549bb53292b31b
I066841 <a.kulshreshtha@sap.com>
2019-09-12 10:20:31 +0530

    Consumer Interface Chaining

---
+++ src/com/learn/java8/main/ConsumerUseCase4.java
@@ -0,0 +1,16 @@
+package com.learn.java8.main;
+
+import java.util.function.Consumer;
+
+public class ConsumerUseCase4 {
+
+	public static void main(String[] args) {
+		String name = "Abhijeet";
+		String lastName="Kulshreshtha";
+		String companyName="ABC Corp.";
+		
+		Consumer<String> c1 = str->System.out.println();
+
+	}
+
+}

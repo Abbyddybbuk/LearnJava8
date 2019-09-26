b7aa3fb074acd66be58b763f958149c052f8e3f8
I066841 <a.kulshreshtha@sap.com>
2019-09-26 12:56:41 +0530

    Method Reference Example ::

---
+++ src/com/learn/java8/main/MethodRefExample.java
@@ -0,0 +1,21 @@
+package com.learn.java8.main;
+
+public class MethodRefExample {
+    
+	public static void main(String[] args) {
+		// Method Reference Example
+		myMethodRef myRefTest = MethodRefExample::m2;
+		myRefTest.m1();
+
+	}
+	
+	public static void m2() {
+		System.out.println("Hello World");
+	}
+
+}
+
+@FunctionalInterface
+interface myMethodRef {
+	public void m1();
+}

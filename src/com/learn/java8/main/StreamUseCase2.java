988a9412c527f564482c66eea0585201f0b6478d
I066841 <a.kulshreshtha@sap.com>
2019-09-27 15:13:36 +0530

    Stream Use Cases: count(), sorted() along with custom Sorting

---
+++ src/com/learn/java8/main/StreamUseCase2.java
@@ -0,0 +1,45 @@
+package com.learn.java8.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUseCase2 {
+
+	public static void main(String[] args) {
+		List<String> nameList = new ArrayList<String>();
+		nameList.add("Abhijeet");
+		nameList.add("Naresh");
+		nameList.add("Sukesh");
+		nameList.add("Amitabh");
+		nameList.add("Abhishek");
+
+		List<String> finalNameList = nameList.stream().filter(name -> name.length() > 6).collect(Collectors.toList());
+
+		System.out.println(finalNameList);
+
+		long countNum = nameList.stream().filter(name -> name.length() > 6).count();
+		System.out.println(countNum);
+
+		// Sorted Example
+		List<Integer> numList = new ArrayList<Integer>();
+		numList.add(0);
+		numList.add(10);
+		numList.add(5);
+		numList.add(20);
+		numList.add(25);
+		numList.add(15);
+		System.out.println("Unsorted List: " + numList);
+
+		// Sort in Ascending order
+		List<Integer> sortedNumList = numList.stream().sorted().collect(Collectors.toList());
+		System.out.println("Sorted List: " + sortedNumList);
+
+		// Sort in Descending order
+		List<Integer> sortedDescList = numList.stream().sorted((elm1, elm2) -> elm2.compareTo(elm1))
+				.collect(Collectors.toList());
+		System.out.println("Sorted List in Descending Order " + sortedDescList);
+
+	}
+
+}

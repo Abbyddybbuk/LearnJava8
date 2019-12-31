97e1545772cf9231f2745f86b4ee8d10662770a0
I066841 <a.kulshreshtha@sap.com>
2019-12-31 13:18:45 +0530

    Lot of Learning

--- src/com/learn/java8/main/DoubleColonP1.java
+++ src/com/learn/java8/main/DoubleColonP1.java
@@ -8,7 +8,18 @@ public class DoubleColonP1 {
 		
 		System.out.println(name);
 		
+		// With Lambda Function
+		MyTestInterf22 myName1 = str -> str.concat("Kulshreshtha");
 		
+		String names = myName1.callMyName("Abhijeet ");
+		
+		System.out.println(names);
+		
+		// With double colon operator
+		MyTestInterf22 myName2 = DoubleColonP1::callMyUpdatedName;
+		String name1 = myName2.callMyName("Abhijeet ");
+		
+		System.out.println(name1);
 
 	}
 	
@@ -16,10 +27,19 @@ public class DoubleColonP1 {
 	public static String callName() {
 		return "Abhijeet Kulshreshtha";
 	}
+	
+	public static String callMyUpdatedName(String name) {
+		return name.concat("Kulshreshtha");
+	}
 
 }
 
 @FunctionalInterface
 interface MyTestInterf {
 	public String callMyName();
+}
+
+@FunctionalInterface
+interface MyTestInterf22 {
+	public String callMyName(String name);
 }
